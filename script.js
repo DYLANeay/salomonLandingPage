24e4b29f55271d69c2527e62e23dbc27003cf3ad
dylan <185331015+DYLANeay@users.noreply.github.com>
2025-02-10 20:24:43 +0100

    Finish

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// menu hamburger
 document.querySelector(".menu-toggle").addEventListener("click", function() {
     document.querySelector(".navbar").classList.toggle("active");
 });
+
+//on cache le footerFixed si il est par dessus le footerNormal
+window.addEventListener('scroll', () => {
+    const fixedFooter = document.querySelector('.fixed');
+    const normalFooter = document.querySelector('.normal');
+
+    const fixedRect = fixedFooter.getBoundingClientRect();
+    const normalRect = normalFooter.getBoundingClientRect();
+
+    if (
+        fixedRect.bottom > normalRect.top &&
+        fixedRect.top < normalRect.bottom
+    ) {
+        fixedFooter.style.display = 'none'; // Masque le footer fixe lorsqu'il est au-dessus du footer normal
+    } else {
+        fixedFooter.style.display = 'fixed'; // Affiche le footer fixe quand il n'est pas au-dessus (marche pas)
+    }
+});
+
+
+
+
